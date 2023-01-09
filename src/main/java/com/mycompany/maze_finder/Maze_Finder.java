6dd78108bed6131199b7a0a7b85745a8f4e2792a
user <user@DESKTOP-60J38I5>
2023-01-09 23:47:50 +0530

    First Commit

---
+++ src/main/java/com/mycompany/maze_finder/Maze_Finder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maze_finder;
+
+/**
+ *
+ * @author user
+ */
+public class Maze_Finder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
